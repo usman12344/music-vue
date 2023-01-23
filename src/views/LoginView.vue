77164d26de20ea75c65c7570be03372bb5d69b63
usman <usmanabdat6@gmail.com>
2023-01-23 16:16:17 +0700

    gitignore

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -48,7 +48,7 @@
             </form>
           </div>
           <div class="w-full mx-auto mt-20 sm:max-w-2xl">
-            <img src="img/brand-logo.svg" alt="" class="w-full px-10 md:px-0" />
+            <img src="" alt="" class="w-full px-10 md:px-0" />
           </div>
         </div>
       </div>
