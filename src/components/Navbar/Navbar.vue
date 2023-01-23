774bf18b95bf25a54616df0af54e7ce65fe8f2d4
usman <usmanabdat6@gmail.com>
2023-01-23 16:32:28 +0700

    test

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -13,7 +13,7 @@ import { RouterLink } from 'vue-router';
       >
         <RouterLink to="/" class="flex items-center">
           <img
-            src="@/assets/img/music 1.svg"
+            src="@/assets/img/logo.svg"
             class="h-8 ml-3 sm:ml-0 sm:h-8"
             alt="Flowbite Logo"
           />
